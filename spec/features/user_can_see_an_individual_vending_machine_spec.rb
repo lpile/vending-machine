43bd96bcd447fba06ffbca1cd4ba8f52a3e491f2
lpile <logan.pile@gmail.com>
2019-05-24 09:48:53 -0600

    Complete User Story 2

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -19,9 +19,22 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
     dons.snacks << [snack_1, snack_2, snack_3]
 
     visit machine_path(dons)
-    save_and_open_page
+
     expect(page).to have_content("#{snack_1.name}: #{snack_1.price}")
     expect(page).to have_content("#{snack_2.name}: #{snack_2.price}")
     expect(page).to have_content("#{snack_3.name}: #{snack_3.price}")
+  end
+
+  scenario 'I also see an average price for all of the snacks in that machine' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack_1 = Snack.create!(name: "Pizza", price: 2.22)
+    snack_2 = Snack.create!(name: "Candy", price: 3.22)
+    snack_3 = Snack.create!(name: "Fish", price: 4.22)
+    dons.snacks << [snack_1, snack_2, snack_3]
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Average Price of Snacks: #{dons.average_price}")
   end
 end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Machine, type: :model do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
   end
+
+  describe 'instance methods' do
+    it ".average_price" do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+      snack_1 = Snack.create!(name: "Pizza", price: 2.22)
+      snack_2 = Snack.create!(name: "Candy", price: 3.22)
+      snack_3 = Snack.create!(name: "Fish", price: 4.22)
+      dons.snacks << [snack_1, snack_2, snack_3]
+
+      expect(dons.average_price).to eq(3.22)
+    end
+  end
 end
